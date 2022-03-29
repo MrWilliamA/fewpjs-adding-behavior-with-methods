79d0c082a98f5cfd23ca454977f9910d63b01285
MrWilliamA <will.archer@hotmail.com>
2022-03-29 20:38:00 +1100

    fin

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() { 
+        return `${this.name} says meow!`;
+    }
+  }
+
+  let Louise = new Cat('Louise');
+  
+  class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() { 
+        return `${this.name} says woof!`;
+}
+  }
+  
+  class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+
+        if (this.sex == 'male') {
+            return `It's me! ${this.name}, the parrot!`;
+
+        } else {
+
+            return `${this.name} says squawk!`
+
+        }
+    }
+  }
